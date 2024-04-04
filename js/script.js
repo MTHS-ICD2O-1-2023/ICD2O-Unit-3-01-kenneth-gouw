87c6b34cc34218c829632fefd61af234763f800a
kenneth-gouw <159185824+kenneth-gouw@users.noreply.github.com>
2024-04-04 18:43:51 +0000

    ttt

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Kenneth All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Kenneth
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function findAreaClicked() {
+  const base1Input = parseInt(document.getElementById("base1-entered").value)
+  const base2Input = parseInt(document.getElementById("base2-entered").value)
+  const heightInput = parseInt(document.getElementById("height-entered").value)
+  const area = ((base1Input * base2Input) / 2) * heightInput
+  document.getElementById("answer").innerHTML = "The area is: " + area.toFixed(2) + " mm<sup>2"
+}
